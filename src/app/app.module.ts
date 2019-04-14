4b5eab5b7fe159dcf2d5fa9ec3ab1ada54c2dc80
kyy Ho <thuonghx@itvsoft.asia>
2019-04-14 07:47:49 +0700

    intergate PrimeNG

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {ColorPickerModule} from 'primeng/colorpicker';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { NameComponent } from './path/name/name.component';
+import { MessageComponent } from './shared/message/message.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NameComponent,
+    MessageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ColorPickerModule,
+    BrowserAnimationsModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  color: string;
+}
